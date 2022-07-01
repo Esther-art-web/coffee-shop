971a6f6051ac76e581ab7cc0f1cf48cd8493bb49
Ibeh Esther <estheribeh5@gmail.com>
2022-07-01 20:13:56 +0100

    setup Auth0, authentication and API of coffee shop application

--- starter_code/backend/src/api.py
+++ starter_code/backend/src/api.py
@@ -4,7 +4,7 @@ from sqlalchemy import exc
 import json
 from flask_cors import CORS
 
-from .database.models import db_drop_and_create_all, setup_db, Drink
+from .database.models import db, db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
@@ -12,12 +12,12 @@ setup_db(app)
 CORS(app)
 
 '''
-@TODO uncomment the following line to initialize the datbase
+@TODO uncomment the following line to initialize the database
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 # ROUTES
 '''
@@ -25,19 +25,45 @@ CORS(app)
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+        returns status code 200 and json {"success": True, "drinks": drinks}
+        where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
 
 
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks')
+@requires_auth('get:drinks')
+def get_all_drinks(payload):
+    try:
+        drinks = Drink.query.all()
+    except Exception:
+        abort(404)
+    return jsonify({
+        'success': True,
+        'drinks': [drink.short() for drink in drinks]
+    })
+# '''
+# @TODO implement endpoint
+#     GET /drinks-detail
+#         it should require the 'get:drinks-detail' permission
+#         it should contain the drink.long() data representation
+#         returns status code 200 and json {"success": True, "drinks": drinks}
+#         where drinks is the list of drinks
+#         or appropriate status code indicating reason for failure
+# '''
+
+
+@app.route('/drinks-detail')
+@requires_auth('get:drinks-detail')
+def get_drinks_detail(payload):
+    try:
+        drinks = Drink.query.all()
+    except Exception:
+        abort(404)
+    return jsonify({
+        'success': True,
+        'drinks': [drink.long() for drink in drinks]
+    })
 
 
 '''
@@ -46,9 +72,34 @@ CORS(app)
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
+        returns status code 200 and json {"success": True,
+        "drinks": drink} where drink an array containing only
+        the newly created drink or appropriate status code
+        indicating reason for failure
 '''
+
+
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def create_new_drink(payload):
+    drinks = []
+    try:
+        body = request.get_json()
+        title = body.get('title')
+        recipe = body.get('recipe')
+        recipe = json.dumps(recipe)
+        new_drink = Drink(title=title, recipe=recipe)
+        new_drink.insert()
+        drinks.append(new_drink.long())
+    except Exception:
+        db.session.rollback()
+        abort(400)
+    finally:
+        db.session.close()
+    return jsonify({
+        'success': True,
+        'drinks': drinks
+    })
 
 
 '''
@@ -59,9 +110,38 @@ CORS(app)
         it should update the corresponding row for <id>
         it should require the 'patch:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+        returns status code 200 and json {"success": True,
+        "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks/<int:id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def update_drink_detail(payload, id):
+    drinks = []
+    try:
+        drink = Drink.query.get(id)
+        body = request.get_json()
+        if drink:
+            for data in body:
+                if data == 'recipe':
+                    setattr(drink, data, json.dumps(body[data]))
+                else:
+                    setattr(drink, data, body[data])
+            drink.update()
+            drinks.append(drink.long())
+        else:
+            abort(404)
+    except Exception:
+        db.session.rollback()
+        abort(422)
+    finally:
+        db.session.close()
+    return jsonify({
+        'success': True,
+        'drinks': drinks
+    })
 
 
 '''
@@ -71,15 +151,53 @@ CORS(app)
         it should respond with a 404 error if <id> is not found
         it should delete the corresponding row for <id>
         it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+        returns status code 200 and json {"success": True,
+        "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks/<int:id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(payload, id):
+    drinks = []
+    try:
+        drink = Drink.query.get(id)
+        drinks.append(drink.long())
+        drink.delete()
+    except Exception:
+        db.session.rollback()
+        abort(404)
+    finally:
+        db.session.close()
+    return jsonify({
+        'success': True,
+        'drinks': drinks
+    })
 
 
 # Error Handling
 '''
 Example error handling for unprocessable entity
 '''
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "bad request"
+    }), 400
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "resource not found"
+    }), 404
 
 
 @app.errorhandler(422)
@@ -112,3 +230,10 @@ def unprocessable(error):
 @TODO implement error handler for AuthError
     error handler should conform to general task above
 '''
+
+
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
--- starter_code/backend/src/database/models.py
+++ starter_code/backend/src/database/models.py
@@ -5,7 +5,9 @@ import json
 
 database_filename = "database.db"
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+database_path = "sqlite:///{}".format(
+    os.path.join(project_dir, database_filename)
+    )
 
 db = SQLAlchemy()
 
@@ -18,16 +20,18 @@ setup_db(app)
 def setup_db(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["PROPAGATE_EXCEPTIONS"] = True
     db.app = app
     db.init_app(app)
 
 
-'''
-db_drop_and_create_all()
-    drops the database tables and starts fresh
-    can be used to initialize a clean database
-    !!NOTE you can change the database_filename variable to have multiple verisons of a database
-'''
+# '''
+# db_drop_and_create_all()
+#     drops the database tables and starts fresh
+#     can be used to initialize a clean database
+#     !!NOTE you can change the database_filename
+#     variable to have multiple verisons of a database
+# '''
 
 
 def db_drop_and_create_all():
@@ -38,11 +42,10 @@ def db_drop_and_create_all():
         title='water',
         recipe='[{"name": "water", "color": "blue", "parts": 1}]'
     )
-
-
     drink.insert()
-# ROUTES
 
+
+# ROUTES
 '''
 Drink
 a persistent drink entity, extends the base SQLAlchemy Model
@@ -55,7 +58,8 @@ class Drink(db.Model):
     # String Title
     title = Column(String(80), unique=True)
     # the ingredients blob - this stores a lazy json blob
-    # the required datatype is [{'color': string, 'name':string, 'parts':number}]
+    # the required datatype is [{'color': string,
+    # 'name':string, 'parts':number}]
     recipe = Column(String(180), nullable=False)
 
     '''
@@ -64,8 +68,11 @@ class Drink(db.Model):
     '''
 
     def short(self):
-        print(json.loads(self.recipe))
-        short_recipe = [{'color': r['color'], 'parts': r['parts']} for r in json.loads(self.recipe)]
+        # print(json.loads(self.recipe))
+        short_recipe = [
+            {'color': r['color'], 'parts': r['parts']}
+            for r in json.loads(self.recipe)
+            ]
         return {
             'id': self.id,
             'title': self.title,
